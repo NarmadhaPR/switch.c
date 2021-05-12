ecc3f14caacbd139aaf280982a4ce654a8887b6e
NarmadhaPR <84066420+NarmadhaPR@users.noreply.github.com>
2021-05-12 15:14:00 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int choice;
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:
+			printf("Pizza,Rs 239");
+			break;
+		case 2:
+			printf("burger,Rs 129");
+			break;
+		case 3:
+			printf("Pasta,Rs 179");
+			break;
+		case 4:
+			printf("french fries,Rs 99");
+			break;
+		case 5:
+			printf("sandwitch,Rs 149");
+			break;
+		
+	}
+}
